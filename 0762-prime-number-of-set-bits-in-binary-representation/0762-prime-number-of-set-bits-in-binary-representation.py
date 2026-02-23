6c78ffbdd8a842b812cc55eee61b342cec59647b
ShengShuYan <109014833+ShengShuYan@users.noreply.github.com>
2026-02-23 14:26:14 +0800

    .py Easy 0762-prime-number-of-set-bits-in-binary-representation 02-23-2026 49 ms (72.62%) 19.5 MB (23.28%)

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.py
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.py
@@ -1,29 +1,10 @@
+# 6 * log2 10 = 6 * 3.32192809489 < 20
 class Solution:
     def countPrimeSetBits(self, left: int, right: int) -> int:
         count = 0
+        p_set = {2, 3, 5, 7, 11, 13, 17, 19}
         for i in range(left, right + 1):
-            count += self.is_prime(i)
+            count += bin(i).count('1') in p_set
 
         return count
 
-    def is_prime(self, a: int) -> int:
-        a_count = bin(a).count('1')
-        if a_count <= 1:
-            return 0
-        if a_count <= 3:
-            return 1
-        if a_count % 2 == 0 or a_count % 3 == 0:
-            return 0
-
-        i = 5
-        while i * i <= a_count:
-            if a_count % i == 0 or a_count % (i+2) == 0:
-                return 0
-            i += 6
-
-        return 1
-
-
-            
-
-        
