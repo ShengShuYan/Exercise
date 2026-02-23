72dc21939ba7bfe55cca3d15eeb26ee7c7380e24
ShengShuYan <109014833+ShengShuYan@users.noreply.github.com>
2026-02-23 14:26:34 +0800

    .py Easy 0762-prime-number-of-set-bits-in-binary-representation 02-23-2026 62 ms (60.79%) 19.5 MB (23.28%)

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.py
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.py
@@ -4,7 +4,7 @@ class Solution:
         count = 0
         p_set = {2, 3, 5, 7, 11, 13, 17, 19}
         for i in range(left, right + 1):
-            count += bin(i).count('1') in p_set
+            count += int(bin(i).count('1') in p_set)
 
         return count
 
