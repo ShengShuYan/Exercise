76677cfe1d887624f782472fa1b595b44293222d
ShengShuYan <109014833+ShengShuYan@users.noreply.github.com>
2026-02-26 15:20:50 +0800

    .py Medium 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one 02-26-2026 3 ms (40.39%) 19.3 MB (80.78%)

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -3,7 +3,7 @@ class Solution:
         s = int(s, 2)
         turns = 0
         while s != 1:
-            if s & 1:
+            if s % 2 == 1:
                 s += 1
             else:
                 s = s >> 1
