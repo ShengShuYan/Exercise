72048126aca33648d6190db46aa0a84bbb3f12ec
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-04-23 15:13:27 +0800

    .py Medium 2615-sum-of-distances 04-23-2026 135 ms (64.17%) 54.2 MB (72.83%)

---
+++ 2615-sum-of-distances/2615-sum-of-distances.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+class Solution:
+    def distance(self, nums: List[int]) -> List[int]:
+        mapd = defaultdict(list)
+        for i in range(len(nums)):
+            mapd[nums[i]].append(i)
+        arr = [0] * len(nums)
+        for v in mapd.values():
+            start = v[0]
+            arr[start] = sum(abs(start - j) for j in v)
+            sv = arr[start]
+            L = len(v)
+            for ind in range(1, len(v)):
+                arr[v[ind]] = sv + (2*ind-L) * (v[ind] - v[ind-1])
+                sv = arr[v[ind]]
+
+        return arr
