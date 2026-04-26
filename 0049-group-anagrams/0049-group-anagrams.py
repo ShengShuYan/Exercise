0879e5297573af4ed8496054eedba105d34a3dc0
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-04-26 17:13:35 +0800

    .py Medium 0049-group-anagrams 04-26-2026 N/A (0%) N/A (0%)

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,10 +3,7 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         ans = defaultdict(list)
         for s in strs:
-            cou = [0] * 26
-            for c in s:
-                cou[ord(c) - ord('a')]+= 1
-            ans[tuple(cou)].append(s)
+            ans[tuple(sorted(s))].append(s)
         return list(ans.values())
 
         
