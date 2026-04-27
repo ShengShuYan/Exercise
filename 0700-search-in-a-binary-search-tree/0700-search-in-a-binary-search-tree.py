12197d38a52b1dd4886260a93951467d4e5f344d
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-04-27 10:34:21 +0800

    .py Easy 0700-search-in-a-binary-search-tree 04-27-2026 0 ms (100%) 20.9 MB (78.45%)

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
@@ -6,10 +6,13 @@
 #         self.right = right
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        if not root: return None
-        if root.val == val: return root
-        if root.val > val:
+        if not root:
+            return None
+        if val == root.val:
+            return root
+        elif val < root.val:
             return self.searchBST(root.left, val)
         else:
             return self.searchBST(root.right, val)
+
         
