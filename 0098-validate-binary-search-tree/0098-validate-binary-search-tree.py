33713295c99a0220438a03eaab5ec4725e07a7d2
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-04-27 16:02:04 +0800

    .py Medium 0098-validate-binary-search-tree 04-27-2026 N/A (0%) N/A (0%)

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -1,9 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         self.pre = -float('inf')
@@ -11,18 +5,17 @@ class Solution:
         def sinorder(node):
             if not node:
                 return True
+            
+            # 如果左子树返回 False，向上层返回 False
             if not sinorder(node.left):
                 return False
+            
+            # 比较当前值与前一个值
             if node.val <= self.pre:
                 return False
             self.pre = node.val
+            
+            # 递归右子树并返回其结果
             return sinorder(node.right)
         
-        
         return sinorder(root)
-
-            
-            
-
-
-        
