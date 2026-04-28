783bbdbe2b660b0089bef29dc634a312a63d7298
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-04-28 11:11:33 +0800

    .py Medium 0399-evaluate-division 04-28-2026 N/A (0%) N/A (0%)

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,35 +1,36 @@
+from collections import defaultdict
+
 class Solution:
-    def calcEquation(self, equations, values, queries):
-        root = {} # 存储父节点
-        dist = {} # 存储 node / parent_node 的比例
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        # 1. 构造图
+        graph = defaultdict(dict)
+        for (u, v), val in zip(equations, values):
+            graph[u][v] = val
+            graph[v][u] = 1.0 / val
+        
+        # 2. 定义 DFS 函数
+        def dfs(start, end, visited):
+            # 如果走到了终点，返回 1.0（倍数之积的基数）
+            if start == end:
+                return 1.0
+            
+            visited.add(start)
+            
+            # 遍历邻居节点
+            for neighbor, weight in graph[start].items():
+                if neighbor not in visited:
+                    res = dfs(neighbor, end, visited)
+                    if res != -1.0:
+                        return weight * res # 路径倍数累乘
+            
+            return -1.0 # 没找到路径
 
-        def find(i):
-            if i not in root:
-                root[i], dist[i] = i, 1.0
-            if root[i] != i:
-                orig_parent = root[i]
-                root[i] = find(orig_parent) # 路径压缩
-                dist[i] *= dist[orig_parent] # 更新比例：node / root
-            return root[i]
-
-        def union(i, j, v):
-            root_i, root_j = find(i), find(j)
-            if root_i != root_j:
-                root[root_i] = root_j
-                # 原理：i/root_i = dist_i, j/root_j = dist_j, i/j = v
-                # 推导：root_i / root_j = (i / dist_i) / (j / dist_j) = v * dist_j / dist_i
-                dist[root_i] = v * dist[j] / dist[i]
-
-        # 1. 建并查集
-        for (i, j), v in zip(equations, values):
-            union(i, j, v)
-
-        # 2. 查询
-        res = []
-        for i, j in queries:
-            if i not in root or j not in root or find(i) != find(j):
-                res.append(-1.0)
+        # 3. 处理查询
+        results = []
+        for c, d in queries:
+            if c not in graph or d not in graph:
+                results.append(-1.0)
             else:
-                # i / j = (i / root) / (j / root)
-                res.append(dist[i] / dist[j])
-        return res
+                results.append(dfs(c, d, set()))
+        
+        return results
