edaca3551850dc00b60ee86ba5ae52c842a2cd77
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-05-03 12:37:45 +0800

    .py Easy 0796-rotate-string 05-03-2026 N/A (0%) N/A (0%)

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,4 +1,4 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        return goal in s+s and len(s) == len(goal)
+        return goal in (s+s) and len(s) == len(goal)
         
