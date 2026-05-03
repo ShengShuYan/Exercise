dc6d8a30f82ce855dcd8f2226fb2fae7209598ed
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-05-03 12:59:30 +0800

    .py Easy 1470-shuffle-the-array 05-03-2026 N/A (0%) N/A (0%)

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,8 +1,8 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         ans = []
-        for i in range(n):
-            ans.append(nums[i])
-            ans.append(nums[i+n])
+        for _ in range(n):
+            ans.append(nums[_])
+            ans.append(nums[n+_])
         return ans
         
