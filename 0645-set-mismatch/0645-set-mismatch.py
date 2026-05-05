56e6567ea2bdbcb1ae1555251d30283460109f3b
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-05-05 09:22:58 +0800

    .py Easy 0645-set-mismatch 05-05-2026 N/A (0%) N/A (0%)

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,23 +1,19 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        nums.sort()
         dup = 0
         mis = 0
-        cur = 1
-
-        for i in range(len(nums)):
-            if nums[i] != cur:
-                if nums[i] == cur - 1:
-                    dup = nums[i]
-                elif nums[i] == cur + 1:                   
-                    mis = cur
-                    cur = cur + 2
-            else:
-                cur = cur + 1
-        
-        if mis == 0:
-            mis = cur
+        rem = [0] * len(nums)
+        for i in nums:
+            rem[i-1] += 1
+        for j in range(len(rem)):
+            if rem[j] == 0:
+                mis = j+1
+            elif rem[j] == 2:
+                dup = j+1
+            if mis and dup:
+                break
         return [dup, mis]
+
 
 
         
