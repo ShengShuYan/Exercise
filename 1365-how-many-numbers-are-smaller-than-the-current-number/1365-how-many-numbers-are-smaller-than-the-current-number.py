1e922f965d0c4de3141fb320b7a139810a80b541
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-05-05 09:32:13 +0800

    .py Easy 1365-how-many-numbers-are-smaller-than-the-current-number 05-05-2026 N/A (0%) N/A (0%)

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -2,10 +2,14 @@ from bisect import bisect_left
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         sort_num = sorted(nums)
-        ans = []
-        for i in nums:
-            ans.append(bisect_left(sort_num, i))
-
+        ans = [0]*len(nums)
+        dic = {}
+        for i in range(len(sort_num)):
+            if sort_num[i] not in dic:
+                dic[sort_num[i]] = i
+        for j in range(len(nums)):
+            ans[j] = dic[nums[j]]
+            
         return ans
 
         
