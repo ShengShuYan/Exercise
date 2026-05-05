704adbb9108f9cedfc79dc4bbc44723b106f3087
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-05-05 09:46:12 +0800

    .py Easy 0448-find-all-numbers-disappeared-in-an-array 05-05-2026 N/A (0%) N/A (0%)

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -1,10 +1,10 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        count = [0] * n
+        count = [0] * (1+n)
         for i in nums:
-            count[i-1] += 1
-        return [i+1 for i in range(n) if count[i] == 0]
+            count[i] = 1
+        return [i for i in range(1, 1+n) if count[i] == 0]
 
 
         
