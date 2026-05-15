1d0b99a427a5665e1f2e1ca565082b561a68db32
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-05-15 21:46:58 +0800

    .py Medium 1441-build-an-array-with-stack-operations 05-15-2026 0 ms (100%) 19.3 MB (25.53%)

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -1,17 +1,15 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
         leng = target[-1]
-        ans = [0] * leng
-        for i in target:
-            ans[i-1] = 1
+        target = set(target)
         out = []
-        for j in ans:
-            if j == 1:
+        for i in range(1, leng+1):
+            if i in target:
                 out.append("Push")
             else:
                 out.extend(["Push", "Pop"])
 
         return out
-        
+
 
         
