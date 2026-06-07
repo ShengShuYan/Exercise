be486986058cd79a7e7db1644574c62ee05f9a83
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-06-07 10:41:28 +0800

    .py Medium 2196-create-binary-tree-from-descriptions 06-07-2026 112 ms (97.36%) 27.1 MB (66.87%)

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
@@ -6,26 +6,22 @@
 #         self.right = right
 class Solution:
     def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
-        P = set()
-        C = set()
+
         v_pool = {}
+        C = set()
         for p, c, i in descriptions:
             if c not in v_pool:
                 v_pool[c] = TreeNode(c)
             if p not in v_pool:
-                if i == 1:
-                    v_pool[p] = TreeNode(p, v_pool[c])
-                else:
-                    v_pool[p] = TreeNode(p, None, v_pool[c])
+                v_pool[p] = TreeNode(p)
+            if i == 1:
+                v_pool[p].left = v_pool[c]
             else:
-                if i == 1:
-                    v_pool[p].left = v_pool[c]
-                else:
-                    v_pool[p].right = v_pool[c]
-            P.add(p)
+                v_pool[p].right = v_pool[c]
+
             C.add(c)
 
-        for p in P:
+        for p in v_pool:
             if p not in C:
                 return v_pool[p]
 
