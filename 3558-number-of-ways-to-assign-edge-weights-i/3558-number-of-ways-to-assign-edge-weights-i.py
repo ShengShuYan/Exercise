286c082779d97e3243342ffcb4eb9f6a9adb5751
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-06-11 21:18:55 +0800

    .py Medium 3558-number-of-ways-to-assign-edge-weights-i 06-11-2026 279 ms (82.8%) 130.8 MB (9.68%)

--- 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.py
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.py
@@ -1,24 +1,30 @@
-from collections import defaultdict
 class Solution:
     def assignEdgeWeights(self, edges: List[List[int]]) -> int:
-
+        n = len(edges) + 1
         parent = defaultdict(list)
 
         for u, v in edges:
-            parent[u].append(v)
-            parent[v].append(u)
+            if u < v:
+                parent[u].append(v)
+            else:
+                parent[v].append(u)
 
-        deep = 0
-        stack = [(1, 0)]
-        visited = {1}
+        depth = {}
 
-        while stack:
-            node, depth = stack.pop()
-            deep = max(deep, depth)
+        def get_depth(node):
+            if node not in parent:
+                return 0
+            if node in depth:
+                return depth[node]
+
+            deep = 0
             for child in parent[node]:
-                if child not in visited:
-                    visited.add(child)
-                    stack.append((child, depth + 1))            
+                deep = max(deep, 1+get_depth(child))
+
+            depth[node] = deep
+            return deep
+
+        ans = get_depth(1)
             
-        return pow(2, deep-1, 10**9 + 7)
+        return pow(2, ans-1, 10**9 +7)
         
