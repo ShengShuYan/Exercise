291ee798e7900893430d2541c327e14e08a32c89
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-07-07 15:12:31 +0800

    .py Easy 3754-concatenate-non-zero-digits-and-multiply-by-sum-i 07-07-2026 1 ms (33.8%) 19.3 MB (51.25%)

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.py
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -1,15 +1,14 @@
 class Solution:
     def sumAndMultiply(self, n: int) -> int:
+        if n == 0:
+            return 0
         L = []
         N = str(n)
         for digit in N:
             if digit != '0':
                 L.append(digit)
 
-        if not L:
-            x = 0
-        else:
-            x = int(''.join(L))
+        x = int(''.join(L))
 
         return x * sum(int(x) for x in L)
         
