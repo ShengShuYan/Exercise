050830ca3e2d63a807e8d9c6881c8ae6949ae5c6
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-09-12 18:09:46 +0800

    .py Easy 1768-merge-strings-alternately 09-12-2026 45 ms (56.05%) 19.4 MB (18.85%)

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,14 +1,16 @@
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
-        merged = []
-        for c1, c2 in zip(word1, word2):
-            merged.append(c1)
-            merged.append(c2)
-        merged.append(word1[len(word2):])
-        merged.append(word2[len(word1):])
-        return ''.join(merged)
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        l1 = len(word1)
+        l2 = len(word2)
+        ans = []
+        if l1 > l2:
+            for i in range(l2):
+                ans.append(word1[i])
+                ans.append(word2[i])
+            return ''.join(ans)+word1[l2:]
+        else:
+            for i in range(l1):
+                ans.append(word1[i])
+                ans.append(word2[i])
+            return ''.join(ans)+word2[l1:]
+        
