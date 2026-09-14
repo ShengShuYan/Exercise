3958f1d011bcdb5afbd7d4ed1a901d70657c2a54
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-09-14 13:07:54 +0800

    .py Easy 1071-greatest-common-divisor-of-strings 09-14-2026 0 ms (100%) 19.2 MB (74.47%)

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -5,9 +5,7 @@ def gcd(a, b):
 
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        l1 = len(str1)
-        l2 = len(str2)
-        common = gcd(l1, l2)
+        common = gcd(len(str1), len(str2))
         
         if str1 + str2 != str2 + str1:
             return ''
