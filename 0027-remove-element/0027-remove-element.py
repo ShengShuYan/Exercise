ac10c6e8639936e8805cf5e8bbb2ffbea8d716e8
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-09-20 10:07:28 +0800

    .py Easy 0027-remove-element 09-20-2026 0 ms (100%) 19.5 MB (19.73%)

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,18 +1,11 @@
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
         l = 0
-        r = len(nums) - 1
-        while l <= r:
-            if nums[l] == val:
-                while r > 0 and nums[r] == val:
-                    nums.pop()
-                    r -= 1
-                if l > r:
-                    break
+        for r in range(len(nums)):
+            if nums[r] != val:
                 nums[l] = nums[r]
-                nums.pop()
-                r -= 1
-            l += 1
-        return r + 1
+                l += 1
+        return l
+
 
         
