9ec6691db92cae390e6e51cee4fe910e3d3a3c5c
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-09-24 10:01:08 +0800

    .py Easy 3550-smallest-index-with-digit-sum-equal-to-index 09-24-2026 3 ms (58.41%) 19.2 MB (67.07%)

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
@@ -1,14 +1,7 @@
 class Solution:
-    def digsum(self, num):
-        ans = 0
-        while num > 0:
-            ans += num % 10
-            num = num // 10
-        return ans
-
     def smallestIndex(self, nums: List[int]) -> int:
         for i, v in enumerate(nums):
-            if self.digsum(v) == i:
+            if sum(map(int, str(v))) == i:
                 return i
 
         return -1
