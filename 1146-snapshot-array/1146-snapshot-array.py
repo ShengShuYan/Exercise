95377952e4b8cbc219cba29a44ecb9073d14b5d7
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-09-25 11:05:28 +0800

    .py Medium 1146-snapshot-array 09-25-2026 172 ms (61.94%) 50.1 MB (18.99%)

---
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -0,0 +1,25 @@
+import bisect
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.arr = [[[0,0]] for _ in range(length)]
+        self.id = 0
+
+    def set(self, index: int, val: int) -> None:
+        if self.arr[index][-1][0] == self.id:
+            self.arr[index][-1][1] = val
+        self.arr[index].append([self.id, val])
+
+    def snap(self) -> int:
+        self.id += 1
+        return self.id - 1
+
+    def get(self, index: int, snap_id: int) -> int:
+        idx = bisect.bisect_right(self.arr[index], [snap_id, float('inf')])
+        return self.arr[index][idx-1][1]
+
+# Your SnapshotArray object will be instantiated and called as such:
+# obj = SnapshotArray(length)
+# obj.set(index,val)
+# param_2 = obj.snap()
+# param_3 = obj.get(index,snap_id)
