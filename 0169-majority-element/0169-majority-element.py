1cfc4a81020311c08f34ecee17902e5340612dc1
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-09-25 10:35:24 +0800

    .py Easy 0169-majority-element 09-25-2026 0 ms (100%) 21.6 MB (12.22%)

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,6 @@
 from collections import Counter
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
-        n = len(nums)
-        tar = n // 2
-        c = Counter(nums)
-        for v, tim in c.items():
-            if tim > tar:
-                return v       
+        nums.sort()
+        return nums[(len(nums)-1)//2]
         
