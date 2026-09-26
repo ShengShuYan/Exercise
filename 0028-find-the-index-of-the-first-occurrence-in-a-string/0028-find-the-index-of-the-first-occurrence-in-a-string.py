5a60a8350846a6f5de8c7559a6b833bc79d1b7e5
Sheng Shuyan <109014833+ShengShuYan@users.noreply.github.com>
2026-09-27 00:06:28 +0800

    .py Easy 0028-find-the-index-of-the-first-occurrence-in-a-string 09-27-2026 4 ms (8.96%) 19.3 MB (28.4%)

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,12 +1,13 @@
-class Solution(object):
-    def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        return haystack.find(needle)
-        
-
-
-        
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        i, j, l1, l2 = 0, 0, len(haystack), len(needle)
+        while i < l1 and j < l2:
+            if haystack[i] != needle[j]:
+                i = i - j + 1
+                j = 0
+            else:
+                i += 1
+                j += 1
+            if j == l2:
+                return i - j
+        return -1
